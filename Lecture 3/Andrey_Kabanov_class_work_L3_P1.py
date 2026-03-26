df98fc3b8723178c6de8cb84f7a3ff6bda4dc780
Andrey Kabanov <a.kabanov1983@gmail.com>
2026-03-26 08:28:00 +0300

    Изменено название фала с Л3

---
+++ Lecture 3/Andrey_Kabanov_class_work_L3_P1.py
@@ -0,0 +1,45 @@
+# # List
+#
+# numbers = [1, 2, 3, 4, 5]  # Сам список не сортированный и может изменяться
+#
+# print(numbers)
+#
+# print(numbers[0])
+#
+# print(numbers[4])
+#
+# print(numbers[-1])
+#
+# print(type(numbers))
+#
+# numbers.append(1000)
+#
+# print(numbers)
+#
+# li = [1, 2, 3, 4, 5]
+#
+# print(len(li))
+#
+# li.append(45)
+#
+# print(li)
+#
+# li.insert(2, 333)
+#
+# print(li)
+#
+# li.reverse()
+#
+# print(li)
+#
+# li.remove(4)
+#
+# print(li)
+#
+# li = li*2
+#
+# print(li)
+#
+# print(li.count(5))
+#
+#
