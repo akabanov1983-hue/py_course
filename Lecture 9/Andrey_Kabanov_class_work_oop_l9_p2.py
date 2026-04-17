93200a11a84b68a707bc1c14612ba4ffe9ba0483
Andrey Kabanov <a.kabanov1983@gmail.com>
2026-04-17 19:00:31 +0300

    Обновления в ооп

---
+++ Lecture 9/Andrey_Kabanov_class_work_oop_l9_p2.py
@@ -0,0 +1,38 @@
+# # OOP
+# # 15 04 2026
+#
+# class Car:
+#     pass
+#
+#
+# class Wallet:
+#     pass
+#
+# # Укаждого пользователя есть имя, телефон, почта
+# class User:
+#     def __init__(self, user_name, user_phone, user_email):
+#         self.name = user_name
+#         self.phone = user_phone
+#         self.email = user_email
+#
+#
+# stas = User("Stas", "375222222", "stas@m.ru")
+#
+# print(stas.name)
+#
+# print(stas.phone)
+#
+# stas.phone = '99999999'
+#
+# print(stas.phone)
+#
+# class Animal:
+#     pass
+#
+#
+# mycar =   Car()
+#
+# mywallet = Wallet()
+
+# OOP
+# 17 04 2026
